98fc3502225d569b4b816192df60a96dfdb48b76
Lucas <luccaslp99@gmail.com>
2021-06-22 11:11:27 -0300

    estruturando validador

---
+++ form/script.js
@@ -0,0 +1,54 @@
+let validator = {
+    handleSubmit: (event) => {
+        event.preventDefault();
+
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for (let i = 0; i < inputs.length; i++) {
+
+            let input = inputs[i];
+            let check = validator.checkInput(input);
+            if (check !== true) {
+                send = false;
+
+                //exibir o erro
+
+                console.log(check)
+            }
+        }
+
+        if(send){
+            form.submit();
+        }
+    },
+    
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules')
+
+        if (rules !==null) {
+            rules =rules.split('|');
+            for (let k in rules) {
+                let rDtails = rules[k].split('=');
+
+                min = 2;
+                switch (rDtails[0]) {
+                    case 'required':
+                        if(input.value == '')
+                            return 'Este campo é obrigatório.';
+                    break;
+                    case 'min' :
+
+                    break;
+                }
+            }
+
+        }
+        
+        return true;
+    }
+};
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validator.handleSubmit);
