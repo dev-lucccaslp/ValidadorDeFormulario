59acd50ce5fb0372c4ed9bf5396854fbf1a71114
Lucas <luccaslp99@gmail.com>
2021-06-24 18:08:04 -0300

    exibindo erro nos campos nao preenchidos

--- form/script.js
+++ form/script.js
@@ -6,16 +6,15 @@ let validator = {
 
         let inputs = form.querySelectorAll('input');
 
+        validator.clearErrors();
+
         for (let i = 0; i < inputs.length; i++) {
 
             let input = inputs[i];
             let check = validator.checkInput(input);
             if (check !== true) {
-                send = false;
-
-                //exibir o erro
-
-                console.log(check)
+                send = false
+                validator.showError(input, check);
             }
         }
 
@@ -47,7 +46,27 @@ let validator = {
         }
         
         return true;
-    }
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement,input.ElementSibling);
+    },
+        clearErrors:() => {
+            let inputs = form.querySelectorAll('input');
+            for (let i = 0; i < inputs.length; i++){
+                inputs[i].style = '';
+            }
+
+            let errorElements = document.querySelectorAll('.error');
+            for (let i = 0; i < errorElements.length; i++) {
+                errorElements[i].remove();
+            }
+        }
 };
 
 let form = document.querySelector('.validator');
